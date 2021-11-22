1fe1eadf53bf1d648ce10562a4f0ad7a5002fc3e
Joel Carpenter <aldosac@hotmail.com>
2021-11-22 10:55:33 -0800

    Merge conflict test

--- 1-Merge-Conflicts/example.js
+++ 1-Merge-Conflicts/example.js
@@ -1,4 +1,5 @@
 
-const someHandlerFn = () => {
-  const changeThis = "Add your text here!  Go ahead, change it to whatever you want."
+const someHandlerFn = () => {;
+
+  console.log(changeThis);
 }
